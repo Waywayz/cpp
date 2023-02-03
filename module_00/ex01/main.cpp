7f2ecaadc8eb6cc404f090811ac26a109d943c25
Roms <romain.vlrt@gmail.com>
2023-02-03 18:38:01 +0100

    module_00 OK

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -1,26 +1,5 @@
 #include "PhoneBook.hpp"
 
-// int main()
-// {
-//     PhoneBook   directory;
-
-//     std::string input;
-//     directory.Welcome();
-//     while (input.compare("EXIT"))
-//     {
-//         if (input.compare("ADD") == 0)
-//             directory.AddContact();
-//         else if (input.compare("SEARCH") == 0)
-//         {
-//             directory.PrintList();
-//             directory.IndexContact();
-//         }
-//         std::cout << "> ";
-//         std::cin >> input;
-//     }
-//     return 0;
-// }
-
 int main()
 {
     PhoneBook   directory;
