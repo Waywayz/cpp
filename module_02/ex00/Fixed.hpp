ad5989b14acaf1bc3035828299d9a09107f451ad
Roms <romain.vlrt@gmail.com>
2023-02-16 19:25:30 +0100

    ok

---
+++ module_02/ex00/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+private:
+
+    int                 _number;
+    static const int    _nb_bits = 8;
+
+public:
+
+    Fixed();
+    Fixed(const Fixed& other);
+    Fixed   &operator=(const Fixed &other);
+    ~Fixed();
+
+    int     getRawBits( void ) const;
+    void    setRawBits( int const raw );
+
+};
+
+#endif
