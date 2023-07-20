ba20e1ef4f74868b2261acc9303588fbb67bfc90
Roms <romain.vlrt@gmail.com>
2023-07-21 00:33:35 +0200

    feature/add module_09

---
+++ module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rovillar <rovillar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 00:27:17 by rovillar          #+#    #+#             */
+/*   Updated: 2023/07/21 00:31:29 by rovillar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE
+#define BITCOINEXCHANGE
+
+#include <map>
+#include <string.h>
+
+class BitcoinExchange
+{
+
+private:
+
+    BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange& other);
+
+public:
+
+};
+
+#endif
